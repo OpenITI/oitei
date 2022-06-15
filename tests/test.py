feee09e8b2a978fdcfc2e37b3c692d879ea01680
Raff Viglianti <raffaeleviglianti@gmail.com>
2022-06-15 16:54:16 -0400

    updated tests

--- tests/test.py
+++ tests/test.py
@@ -11,29 +11,31 @@ class TestStringMethods(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestStringMethods, self).__init__(*args, **kwargs)
 
-    # def test_generic(self):
-    #     root = os.path.dirname(__file__)
-    #     filepath = os.path.join(
-    #         root, "test.md"
-    #     )
-    #     test_file = open(filepath, "r")
-    #     self.text = test_file.read()
-    #     test_file.close()
-    #     self.converted = oitei.convert(self.text).tostring()
-    #     with open(os.path.join(root, "test.xml"), 'w') as writer:
-    #         writer.write(self.converted)
-
-    def test_ernst(self):
+    def test_generic(self):
         root = os.path.dirname(__file__)
         filepath = os.path.join(
-            root, "ernst_jogiyan_markdown"
+            root, "test.md"
         )
         test_file = open(filepath, "r")
         self.text = test_file.read()
         test_file.close()
         self.converted = oitei.convert(self.text).tostring()
-        with open(os.path.join(root, "ernst_jogiyan_markdown.xml"), 'w') as writer:
+        with open(os.path.join(root, "test.xml"), 'w') as writer:
             writer.write(self.converted)
+
+    # def test_ernst(self):
+    #     root = os.path.dirname(__file__)
+    #     filepath = os.path.join(
+    #         root, "ernst_jogiyan_markdown"
+    #     )
+    #     test_file = open(filepath, "r")
+    #     self.text = test_file.read()
+    #     test_file.close()
+    #     self.converted = oitei.convert(self.text).tostring()
+    #     self.converted = self.converted.replace('&lt;', '<')
+    #     self.converted = self.converted.replace('&gt;', '>')
+    #     with open(os.path.join(root, "ernst_jogiyan_markdown.xml"), 'w') as writer:
+    #         writer.write(self.converted)
 
 
 if __name__ == "__main__":
